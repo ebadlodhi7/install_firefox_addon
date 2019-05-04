0dd64491a578ed9234fc6728357d036499cd8de6
sf44tdw <sf44tdw@users.noreply.github.com>
2019-05-04 14:23:51 +0900

    CentOSのyumでインストールできるアドオンを除外

--- install_firefox_addon.sh
+++ install_firefox_addon.sh
@@ -6,8 +6,6 @@ cd && rm -rf "${FFDIR}" && mkdir "${FFDIR}" && cd "${FFDIR}"|| exit 1
 ADDON_LIST="firefox_add_on.txt"
 #追加したいアドオンのURLをヒアドキュメントに記載する。
 HOGE=$(cat << EOS
-https://addons.mozilla.org/firefox/downloads/file/1672871/ublock_origin-1.18.4-an+fx.xpi
-https://addons.mozilla.org/firefox/downloads/file/1669416/https_everywhere-2019.1.31-an+fx.xpi
 https://addons.mozilla.org/firefox/downloads/file/864868/no_coin-0.4.13-an+fx.xpi
 EOS
 )
