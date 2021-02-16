f7170ca655887f0e8effa58c63b4859a2b5c95c3
Ebad Lodhi <ebadlodhi7@gmail.com>
2021-02-16 15:54:34 +0500

    Update install_firefox_addon.sh
    translation

--- install_firefox_addon.sh
+++ install_firefox_addon.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-echo "";echo `date`;echo "firefoxのアドオンインストール。"
+echo "";echo `date`;echo "firefox Add-on installation"
 FFDIR="${HOME}/ffaddon_temp"
 cd && rm -rf "${FFDIR}" && mkdir "${FFDIR}" && cd "${FFDIR}"|| exit 1
 
 ADDON_LIST="firefox_add_on.txt"
-#追加したいアドオンのURLをヒアドキュメントに記載する。
+#Enter the URL of the add-on you want to add in the here document.
 HOGE=$(cat << EOS
 https://addons.mozilla.org/firefox/downloads/file/864868/no_coin-0.4.13-an+fx.xpi
 EOS
 )
 echo "${HOGE}" | sort | uniq > "${ADDON_LIST}"
 if [ $? -gt 0 ]; then
-   echo "アドオンリスト取得失敗。"
+   echo "Add-on list acquisition failure."
    exit 2
 fi
 
@@ -22,5 +22,5 @@ do
 done < "${ADDON_LIST}"
 
 firefox *.xpi || exit 4
-#firefox側でアドオンのインストール操作が必要なので-fなし。
+#firefox There is no -f because the add-on installation operation is required on the side.
 cd && rm -r "${FFDIR}" || exit 5
